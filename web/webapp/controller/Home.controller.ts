97f504fa07f88965508a09eabd41711b10b312d1
tindezeeuw <tin.de.zeeuw@alliander.com>
2020-08-27 15:08:53 +0200

    Home.controller omgebouwd naar TS

---
+++ web/webapp/controller/Home.controller.ts
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "ui5typescriptdemo/service/DingenService"
+], function (Controller, DingenService) {
+    "use strict";
+
+    const className = "ui5typescriptdemo.controller.Home";
+    
+    class HomeController extends Controller {
+
+        constructor() {
+            super(className);
+            const fnClass = Controller.extend(className, {});
+            this.getMetadata = fnClass.prototype.getMetadata;
+        }
+
+        async onInit() {
+            const dataModel = await DingenService.getDingen();
+            this.getView().setModel(dataModel);
+        }
+    }
+
+    return HomeController;
+});
